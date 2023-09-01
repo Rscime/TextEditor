a5aa65eb21837c700847da2e669e0e853640a801
Rscime <Rachelscime@gmail.com>
2023-09-01 12:34:45 -0400

    completed install code, now deploy to heroku and update readme

--- Develop/client/src/js/index.js
+++ Develop/client/src/js/index.js
@@ -24,10 +24,26 @@ if (typeof editor === 'undefined') {
 }
 
 // Check if service workers are supported
+// if ('serviceWorker' in navigator) {
+//   // register workbox service worker
+//   const workboxSW = new Workbox('/src-sw.js');
+//   workboxSW.register();
+// } else {
+//   console.error('Service workers are not supported in this browser.');
+// }
+
+// register SW with scope
 if ('serviceWorker' in navigator) {
-  // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
-  workboxSW.register();
-} else {
-  console.error('Service workers are not supported in this browser.');
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
+
+this.addEventListener('fetch', function (event) {
+
+});
+
